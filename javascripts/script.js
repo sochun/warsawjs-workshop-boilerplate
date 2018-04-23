2c6e1c7d48c0c7593fd85de8bc63909518e27b9a
Piotr <33068320+sochun@users.noreply.github.com>
2018-04-23 07:51:06 +0200

    Add files via upload

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,22 @@
-$('#suspicious-button').click(function() {
-  alert("YOU'VE JUST CLICKED ME!")
+$("#button1").click(function(){
+    $('<input type="number" class="form-control margin-top text-center liczba" />').appendTo('.kontener')
+});
+
+$("#button2").click(function(){
+   var suma = 0
+   var isValid = true
+   
+$('.liczba').each(function() {
+    var value = $(this).val()
+  if (!value) {
+    isValid = false
+  }
+  suma += Number($(this).val())
+})
+    if (isValid) {
+  alert(suma)
+       $(".kontener").empty()
+} else {
+  alert('Uzupelnj wszystkie pola')
+}
 });
